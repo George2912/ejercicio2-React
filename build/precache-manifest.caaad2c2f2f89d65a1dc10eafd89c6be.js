771f823257ed933fac4fee34dcd4ce7dfb52ecd0
jorge salcido <jorgesalcido399@gmail.com>
2020-11-05 19:17:28 -0700

    se hizo el build

---
+++ build/precache-manifest.caaad2c2f2f89d65a1dc10eafd89c6be.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1760710c892c6de5166af5ba6377ac43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d62544ff34ba9d8b80f",
+    "url": "/static/css/main.3329c7ae.chunk.css"
+  },
+  {
+    "revision": "eb4d3351923d023325ea",
+    "url": "/static/js/2.1befed8e.chunk.js"
+  },
+  {
+    "revision": "4d62544ff34ba9d8b80f",
+    "url": "/static/js/main.6e16c8a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "047ec5024f7ee8b06cd9748af04fc072",
+    "url": "/static/media/video1.047ec502.jpg"
+  },
+  {
+    "revision": "a5c807d362298bec3e2d00edf96539f4",
+    "url": "/static/media/video2.a5c807d3.jpg"
+  },
+  {
+    "revision": "7e7f76cfaf403d893ea6ac139f2291f0",
+    "url": "/static/media/video3.7e7f76cf.jpg"
+  },
+  {
+    "revision": "97ff9a0edccff379b0a4762b0ec4f61f",
+    "url": "/static/media/video4.97ff9a0e.jpg"
+  },
+  {
+    "revision": "8c637443a781f9e9267c49915ad15343",
+    "url": "/static/media/video5.8c637443.jpg"
+  },
+  {
+    "revision": "670478eddd48e6f37b1dd1202849df28",
+    "url": "/static/media/video6.670478ed.jpg"
+  },
+  {
+    "revision": "738cc3c7a6a9fd6110d2743b71410a8f",
+    "url": "/static/media/video7.738cc3c7.jpg"
+  },
+  {
+    "revision": "eda8810232b413a517f136ca4b1c3896",
+    "url": "/static/media/video8.eda88102.jpg"
+  }
+]);
